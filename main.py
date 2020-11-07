789461de3578a49ff9ecec70052d362b6eb5e07a
rkrylov88 <59621679+rkrylov88@users.noreply.github.com>
2020-11-07 16:08:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -353,31 +353,50 @@
 # Ваша задача состоит в том, чтобы решить несколько более общую задачу – а именно по числу n
 # найти количество простых чисел p из интервала n < p < 2n.
 
-def isprime(i):
-    if i < 0:
-        i = -1 * i
-    s = []
-    count = 1
-    if i == 0:
-        return True
-    elif i == 1:
-        return True
-    else:
-        while count * count <= i:
-            if i % count == 0:
-                s.append(count)
-            count += 1
-        if len(s) > 1:
-            return False
-        else:
-            return True
+# def isprime(i):
+#     if i < 0:
+#         i = -1 * i
+#     s = []
+#     count = 1
+#     if i == 0:
+#         return True
+#     elif i == 1:
+#         return True
+#     else:
+#         while count * count <= i:
+#             if i % count == 0:
+#                 s.append(count)
+#             count += 1
+#         if len(s) > 1:
+#             return False
+#         else:
+#             return True
+#
+# n = int(input())
+# my_list = []
+# for i in range(n+1, 2 * n):
+#     if isprime(i):
+#         my_list.append(i)
+# print(len(my_list))
 
-n = int(input())
-my_list = []
-for i in range(n+1, 2 * n):
-    if isprime(i):
-        my_list.append(i)
-print(len(my_list))
+# Программа получает на вход число n - количество элементов в списке, и затем в следующей строке сам список.
+# Ваша задача отсортировать список по возрастанию при помощи сортировки вставками,
+# в случае если элементы соседние совпадают менять их ненужно.
+# В качестве ответа нужно вывести отсортированный список.
+
+count = int(input())
+nums = list(map(int, input().split()))
+for i in range(1, len(nums)):
+    item_to_insert = nums[i]
+    j = i - 1
+    while j >= 0 and nums[j] > item_to_insert:
+        nums[j + 1] = nums[j]
+        j -= 1
+    nums[j + 1] = item_to_insert
+for i in range(len(nums)):
+    nums[i] = str(nums[i])
+s = " ".join(nums)
+print(s)
 
 
 
